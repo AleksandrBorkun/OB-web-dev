98379c121efd2b7e83244f58c7b4cedfb231ad58
Aleksandr Borkun <alexandr.borkun1992@gmail.com>
2022-08-23 22:30:05 +0200

    add analytics and search check

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+const isLocal = process.env.LOCAL_ENV;
 export default function Document() {
   return (
     <Html>
       <Head>
+      {/* <!-- Google tag (gtag.js) --> */}
+      {!isLocal && <meta name="google-site-verification" content="_Mj2o2Y4toCnhEhzYpsG9_P1hhAQkQzGIWK7ExsZv4g" />}
+      {!isLocal && <script async src="https://www.googletagmanager.com/gtag/js?id=G-3P12M9V37J"></script>}
+      {!isLocal && <script dangerouslySetInnerHTML={`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-3P12M9V37J');`}/>}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
