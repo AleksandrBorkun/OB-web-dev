d8d81882367da8537da1b86d0c430dd08d98b44e
Oleksandr Borkun <oleksandr.borkun@lego.com>
2022-11-24 22:24:59 +0100

    add border and padding to imageWithBtn

--- components/ImageWithButton.js
+++ components/ImageWithButton.js
@@ -9,11 +9,18 @@ const ImageWithButton = ({
   buttonText,
   buttonLink,
 }) => (
-  <Box height={'100%'}>
-    <ImageConstructor {...image.fields} styles={`max-height : 350px;`}/>
-    <Typography component={"h2"} variant='h4'>{title}</Typography>
-    <Typography component={"h4"} variant='h6' sx={{marginTop: 2}}>{description}</Typography>
-    <Button variant="text" href={buttonLink} size='large' sx={{marginTop: 4}}>
+  <Box height={"100%"} pr={2} pl={2}>
+    <ImageConstructor
+      {...image.fields}
+      styles={`max-height : 350px;  border-radius: 20px;`}
+    />
+    <Typography component={"h2"} variant="h4">
+      {title}
+    </Typography>
+    <Typography component={"h4"} variant="h6" sx={{ marginTop: 2 }}>
+      {description}
+    </Typography>
+    <Button variant="text" href={buttonLink} size="large" sx={{ marginTop: 4 }}>
       {buttonText}
     </Button>
   </Box>
